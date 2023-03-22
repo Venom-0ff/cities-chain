814b4c4d6bb91c9602b29afbab9f99ed271bc949
Venom-0ff <90479045+Venom-0ff@users.noreply.github.com>
2023-03-21 23:05:01 -0400

    Add host service.

---
+++ CitiesChainHostService/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ServiceModel;
+using CitiesChainLibrary;
+
+namespace CitiesChainHostService
+{
+    /// <summary>
+    /// A host application for the CitiesChain WCF service.
+    /// </summary>
+    internal class Program
+    {
+        static void Main()
+        {
+            ServiceHost serviceHost = null;
+            try
+            {
+                serviceHost = new ServiceHost(typeof(CitiesChain));
+
+                serviceHost.Open();
+                Console.WriteLine("Service Host is running! Press any key to shut down.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+                serviceHost?.Close();
+            }
+        }
+    }
+}
